8cefcc5d8dd40b0e97a3dad9aaeb330c64bfe810
recepozturk53 <recepozturk5341@outlook.com>
2022-02-15 11:31:31 +0300

    still thinking on it

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -26,49 +26,47 @@ class _MainPageState extends State<MainPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Container(
-          child: ClipRRect(
-            borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-            child: Scaffold(
+        child: ClipRRect(
+          borderRadius: const BorderRadius.all(Radius.circular(20.0)),
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              brightness: Brightness.light,
               backgroundColor: Colors.white,
-              appBar: AppBar(
-                brightness: Brightness.light,
-                backgroundColor: Colors.white,
-                elevation: 0,
-                textTheme: const TextTheme(
-                    subtitle1: TextStyle(
-                        color: Colors.pink,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18.0)),
-                iconTheme: const IconThemeData(color: Colors.pink),
-                leading: IconButton(
-                  icon: const Icon(Icons.menu),
-                  onPressed: () {},
-                ),
-                actions: <Widget>[_buildTextButton("Settings", true)],
+              elevation: 0,
+              textTheme: const TextTheme(
+                  subtitle1: TextStyle(
+                      color: Colors.pink,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0)),
+              iconTheme: const IconThemeData(color: Colors.pink),
+              leading: IconButton(
+                icon: const Icon(Icons.menu),
+                onPressed: () {},
               ),
-              body: Column(
-                children: <Widget>[
-                  Expanded(
-                    child: Center(
-                      child: SpringySlider(
-                          markCount: 12,
-                          positiveColor: Colors.lightBlue,
-                          negativeColor: Colors.white),
-                    ),
+              actions: <Widget>[_buildTextButton("Settings", true)],
+            ),
+            body: Column(
+              children: <Widget>[
+                Expanded(
+                  child: Center(
+                    child: SpringySlider(
+                        markCount: 12,
+                        positiveColor: Colors.lightBlue,
+                        negativeColor: Colors.white),
                   ),
-                  Container(
-                    color: Colors.pink,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        _buildTextButton("More", false),
-                        _buildTextButton("Stats", false)
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                ),
+                Container(
+                  color: Colors.pink,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      _buildTextButton("More", false),
+                      _buildTextButton("Stats", false)
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
         ),
